7a28f3f49849bf8d0a026d37ec2e5d5a82f6996c
gowtham adi <gowthamk362@gmail.com>
2024-08-28 15:24:15 +0530

    project initial setup

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native' 
+
+function App(){
+   return(<SafeAreaView>
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+ </SafeAreaView>)
+}
+
+export default App;
